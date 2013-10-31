40f62935682f23d7d5eba64b79c157b795e1e72d
Appie Verschoor <xiffy@xiffy.nl>
2013-10-31 22:27:19 +0100

    Bot iets slimmer maken, er is blijkbaar een limiet op het request aantal dat je kan doen

--- fb-bot.php
+++ fb-bot.php
@@ -6,30 +6,40 @@ require_once('settings.local.php');
 // database, mysql, why not?
 include('db.php');
 
+// nieuwe artikelen eerst!
+$artikelen_res = mysql_query('select * from artikelen left outer join facebook on artikelen.id = facebook.art_id where facebook.art_id IS NULL');
+echo 'Indexing fresh articles. ('.mysql_num_rows($artikelen_res).')';
+crawl($artikelen_res);
+// vervolgens artikelen die lang geleden een update kregen
+$artikelen_res = mysql_query('select * from artikelen left outer join facebook on artikelen.id = facebook.art_id where facebook.id > 0 order by facebook.last_crawl');
+echo 'Updating articles. ('.mysql_num_rows($artikelen_res).')';
+crawl($artikelen_res);
 
-$artikelen_res = mysql_query('select * from artikelen');
-$i = 0;
-while ($artikel = mysql_fetch_array($artikelen_res))
+function crawl($artikelen_res)
 {
-	$fql  = "SELECT url, normalized_url, share_count, like_count, comment_count, ";
-	$fql .= "total_count, commentsbox_count, comments_fbid, click_count FROM ";
-	$fql .= "link_stat WHERE url = '".$artikel['clean_url']."'";
+	while ($artikel = mysql_fetch_array($artikelen_res))
+	{
+		$fql  = "SELECT url, normalized_url, share_count, like_count, comment_count, ";
+		$fql .= "total_count, commentsbox_count, comments_fbid, click_count FROM ";
+		$fql .= "link_stat WHERE url = '".$artikel['clean_url']."'";
 echo 'Querying facebook for: '.$artikel['clean_url']."\n";
-	$apifql="https://api.facebook.com/method/fql.query?format=json&query=".urlencode($fql);
-	$json=file_get_contents($apifql);
-	//print_r($json);
-	$response = json_decode($json);
+		$apifql="https://api.facebook.com/method/fql.query?format=json&query=".urlencode($fql);
+		$json=file_get_contents($apifql);
+		//print_r($json);
+		$response = json_decode($json);
 
-	// now find the record for this article
-	$fb_res = mysql_query('select ID from facebook where art_id = '.$artikel['ID']);
-	if(mysql_num_rows($fb_res) > 0)
-	{
-		mysql_query('update facebook set share_count = '.$response[0]->share_count.', comment_count = '.$response[0]->comment_count.', like_count = '.$response[0]->like_count.', total_count = '.$response[0]->total_count.', click_count = '.$response[0]->click_count.' where art_id = '.$artikel['ID']);
-	}
-	else
-	{
-		mysql_query('insert into facebook (art_id, share_count, comment_count, like_count, total_count, click_count)
-								 values
-								 ('.$artikel['ID'].', '.$response[0]->share_count.', '.$response[0]->comment_count.', '.$response[0]->like_count.', '.$response[0]->total_count.', '.$response[0]->click_count.')');
+		// now find the record for this article
+		$fb_res = mysql_query('select ID from facebook where art_id = '.$artikel['ID']);
+		if(mysql_num_rows($fb_res) > 0)
+		{
+			mysql_query('update facebook set share_count = '.$response[0]->share_count.', comment_count = '.$response[0]->comment_count.', like_count = '.$response[0]->like_count.', total_count = '.$response[0]->total_count.', click_count = '.$response[0]->click_count.' where art_id = '.$artikel['ID']);
+		}
+		else
+		{
+			mysql_query('insert into facebook (art_id, share_count, comment_count, like_count, total_count, click_count)
+									 values
+									 ('.$artikel['ID'].', '.$response[0]->share_count.', '.$response[0]->comment_count.', '.$response[0]->like_count.', '.$response[0]->total_count.', '.$response[0]->click_count.')');
+		}
+
 	}
 }

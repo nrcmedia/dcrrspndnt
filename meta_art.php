425e048c5f86a43945f534665ed2380066e5d87f
Appie Verschoor <xiffy@xiffy.nl>
2013-11-03 15:14:08 +0100

    en ook deze gebruikt no show_table

--- meta_art.php
+++ meta_art.php
@@ -11,7 +11,7 @@ $meta_row = mysql_fetch_array($meta_res);
 $mode = explode(':', $meta_row['type']);
 $mode = isset($mode[1]) ? $mode[1] : $mode;
 $title_by_in = $mode == 'author' ? 'door' : 'in de sectie';
-$th_extra = $mode == 'author' ? 'sectie' : 'auteur';
+$th_extra = $mode == 'author' ? '<th>sectie</th>' : '<th>auteur</th>';
 $th_related = $mode == 'author' ? 'auteurs' : 'secties';
 $extra_query_var = $mode == 'author' ? 'article:section' : 'article:author';
 
@@ -41,6 +41,10 @@ if(isset($_GET['order']) && $_GET['order'] == 'tweets')
 	$th_tweets = '<th>tweets</th>';
 }
 
+
+
+$th = $th_pubdate.'<th>Title / Article</th>'.$th_extra.$th_tweets.'<th>FB</th>';
+
 ?>
 <html>
 	<head>
@@ -56,73 +60,34 @@ if(isset($_GET['order']) && $_GET['order'] == 'tweets')
 <?php include('menu.php')?>
 		<div class="clear"></div>
 
-		<table class="meta-table">
-			<tr>
-				<?php echo $th_pubdate;?>
-				<th>Title / Artikel</th>
-				<th><?php echo $th_extra; ?></th>
-				<?php echo $th_tweets;?>
-			</tr>
 <?php
-$i = 0;
-$art_res = mysql_query ('select artikelen.*, count(tweets.id) as tweet_count from artikelen join meta_artikel on artikelen.ID = meta_artikel.art_id left outer join tweets on tweets.art_id = artikelen.ID where meta_artikel.meta_id = '.$meta_id.' group by artikelen.ID '.$order_by.' limit '.$start.','.ITEMS_PER_PAGE);
+$res = mysql_query ('select artikelen.*, count(tweets.id) as tweet_count, facebook.total_count as fb_total, facebook.share_count as fb_share, facebook.like_count as fb_like, facebook.comment_count as fb_comment from artikelen join meta_artikel on artikelen.ID = meta_artikel.art_id left outer join tweets on tweets.art_id = artikelen.ID left outer join facebook on facebook.art_id = artikelen.id where meta_artikel.meta_id = '.$meta_id.' group by artikelen.ID '.$order_by.' limit '.$start.','.ITEMS_PER_PAGE);
 
-while($row = mysql_fetch_array($art_res))
-{
-	$og = unserialize(stripslashes($row['og']));
-	$titel = isset($og['title']) ? $og['title'] : substr($row['clean_url'],18,50);
-	$description = isset($og['description']) ? $og['description'] : 'Een mysterieus artikel';
-	$display_time = isset($og['article:published_time']) ? strftime('%e %b %H:%M', $og['article:published_time']) : substr($row['created_at'],8,2).'-'.substr($row['created_at'],5,2).' '.substr($row['created_at'],11,5);
-	if(isset($og['article:published_time']) && $og['article:published_time'] < time() - 360 * 24 * 60 * 60)
-		$display_time = strftime('%e %b %Y', $og['article:published_time']);
+if($mode == 'auhor')
+	$fields = array('pubdate', 'title', 'section', 'tweets', 'fb_count');
+else
+	$fields = array('pubdate', 'title', 'author', 'tweets', 'fb_count');
 
-	$found_at = substr($row['created_at'],8,2).'-'.substr($row['created_at'],5,2).' '.substr($row['created_at'],11,5);
+$extra_class = ' class="meta-table"';
+show_table($res, $th, false, $fields, $extra_class);
 
-	$r = mysql_query ('select * from meta_artikel left join meta on meta.ID = meta_artikel.meta_id where meta_artikel.art_id = '.$row['ID'].' and meta.type = "'.$extra_query_var.'"');
-	$extra_arr = mysql_fetch_array($r);
-	?>
-	<tr <?php if($i % 2 == 1) echo 'class="odd"'?>>
-		<td><abbr title="gevonden op: <?php echo $found_at;?>"><?php echo $display_time ?></abbr></td>
-		<td style="max-width:400px"><a href="<?php echo $row['share_url'];?>" title="<?php echo $description ?>"><?php echo $titel ;?></a></td>
-		<td><a href="./meta_art.php?id=<?php echo $extra_arr['ID'];?>"><?php echo $extra_arr['waarde'] ?></a></td>
-		<td align="right"><?php echo $row['tweet_count'];?></td>
-	</tr>
-<?php
-	$i++;
-}
+pager($tot_row, $qsa);
 
-?>
-		</table>
-<?php
-	pager($tot_row, $qsa);
-?>
-
-		<table class="related">
-			<tr><th>Alle <?php echo $th_related;?></th></tr>
-<?php
-			$i = 0;
-			$metatype_res = mysql_query('select * from meta where meta.type = "'.$meta_row['type'].'" order by waarde');
-			while($rel_row = mysql_fetch_array($metatype_res))
-			{
-				?>
-				<tr <?php if($i % 2 == 1) echo 'class="odd"'?>>
-					<td><a href="./meta_art.php?id=<?php echo $rel_row['ID']?>"><?php echo $rel_row['waarde'] ?></a></td>
-				</tr>
-				<?php
-				$i++;
-			}
-?>
-		</table>
-		<div class="center">
-<?php include('search_box.php'); ?>
-<?php
 // grafiekje tweets per dag over deze meta (auteur of sectie)
-$graph_res = mysql_query("select count(tweets.id) as tweet_count, day(tweets.created_at) as dag, month(tweets.created_at) as maand from artikelen join meta_artikel on artikelen.ID = meta_artikel.art_id left outer join tweets on tweets.art_id = artikelen.ID where meta_artikel.meta_id = {$meta_id} and not day(tweets.created_at) is null group by maand, dag");
+$graph_res = mysql_query("select count(tweets.id) as tweet_count, day(tweets.created_at) as dag, month(tweets.created_at) as maand from artikelen join meta_artikel on artikelen.ID = meta_artikel.art_id left outer join tweets on tweets.art_id = artikelen.ID where meta_artikel.meta_id = {$meta_id} and not day(tweets.created_at) is null group by year(tweets.created_at), maand, dag order by year(tweets.created_at) desc, month(tweets.created_at) desc, day(tweets.created_at) desc limit 0,30");
+
+$rows = array();
+while ($row = mysql_fetch_array($graph_res))
+{
+	$rows[] = $row;
+}
+$rows = array_reverse($rows);
+
 
 $high = 0;
 $bar_label = '';
 $bar_tweet_data = '';
-while ($row = mysql_fetch_array($graph_res))
+foreach($rows as $row)
 {
 	$bar_label .= $row['dag'].',';
 	$bar_tweet_data .= $row['tweet_count'].',';
@@ -132,9 +97,9 @@ $scaleWidth = ceil($high / 10);
 $bar_label = substr($bar_label, 0, strlen($bar_label) - 1);
 $bar_tweet_data = substr($bar_tweet_data, 0, strlen($bar_tweet_data) - 1);
 ?>
-			<div class="meta_graph">
-			<h2 id="grafiek">tweets per dag; artikelen <?php echo $title_by_in; ?>: <?php echo $meta_row['waarde'] ?></h2>
-			<canvas id="tot_tweets" height="450" width="800"></canvas>
+			<div class="meta_graph" style="float:left;">
+
+			<canvas id="tot_tweets" height="450" width="650"></canvas>
 			<script>
 				var barOptions = {
 					scaleOverride : 1,
@@ -157,6 +122,27 @@ $bar_tweet_data = substr($bar_tweet_data, 0, strlen($bar_tweet_data) - 1);
 			</script>
 		</div>
 		</div>
+
+		<table class="related">
+			<tr><th>Alle <?php echo $th_related;?></th></tr>
+<?php
+			$i = 0;
+			$metatype_res = mysql_query('select * from meta where meta.type = "'.$meta_row['type'].'" order by waarde');
+			while($rel_row = mysql_fetch_array($metatype_res))
+			{
+				?>
+				<tr <?php if($i % 2 == 1) echo 'class="odd"'?>>
+					<td><a href="./meta_art.php?id=<?php echo $rel_row['ID']?>"><?php echo $rel_row['waarde'] ?></a></td>
+				</tr>
+				<?php
+				$i++;
+			}
+?>
+		</table>
+		<div class="center">
+<?php include('search_box.php'); ?>
+		</div>
+	</div>
 <?php include('footer.php') ?>
 	</body>
 </html>

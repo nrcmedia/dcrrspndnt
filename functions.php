5fe65da5f189849e74eafcb21b6ca9a6d838ca74
Appie Verschoor <xiffy@xiffy.nl>
2013-11-07 02:03:18 +0100

    En daarom ALTIJD accolades na een if

--- functions.php
+++ functions.php
@@ -53,7 +53,11 @@ while($row = mysql_fetch_array($res) )
 	$section_res = mysql_query('select * from meta where meta.waarde = "'.$og['article:section'].'" and meta.type="article:section"');
 	$section = mysql_fetch_array($section_res);
 	$display_time = ! empty($og['article:published_time']) ? strftime('%e %b %H:%M', $og['article:published_time']) : substr($row['created_at'],8,2).'-'.substr($row['created_at'],5,2).' '.substr($row['created_at'],11,5);
+
 	if(isset($og['article:published_time']) && $og['article:published_time'] < time() - 360 * 24 * 60 * 60)
+	{
+		$display_time = strftime('%e %b %Y', $og['article:published_time']);
+	}
 	$found_at = substr($row['created_at'],8,2).'-'.substr($row['created_at'],5,2).' '.substr($row['created_at'],11,5);
 	$fb_abbr = 'Facebook, likes: '.$row['fb_like'].' shares: '.$row['fb_share'].' comments: '.$row['fb_comment'];
 	$tot_tweets += $row['tweet_count'];
